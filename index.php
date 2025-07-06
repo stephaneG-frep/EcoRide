2e1e81e9669d29ae940714f415c061f4da485a61
stephaneG-frep <hoopscmoi@gmail.com>
2025-07-06 10:50:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>EcoRide</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <header>
+        <div class="header">
+            <ul>
+                <li>Accueil</li>
+                <li>Parcours</li>
+                <li>A-propos</li>
+            </ul>
+        </div>
+    </header>
+    <body>
+        <h1>Bienvenue sur EcoRide le site du covoiturage écolo</h1>
+    </body>
+    <footer>
+        <ul>
+            <li>Email:-Nous contacter-</li>
+            <li>Menssion légal</li>
+        </ul>
+    </footer>
+</html>
